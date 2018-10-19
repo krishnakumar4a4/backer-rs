5f230616552b7ecf469ea0d6bfe7b890d57f22e3
krishna kumar T <krishna.thokala2010@gmail.com>
2018-10-19 23:22:15 +0530

    [Krishna] Implemented monitor and delayed commit functionality (#2)
    Done:
    - Integrated with clap to parse command line args
    - Possible to mention file watcher freq and commit delay as args
    - Possible to pass custom commit message
    
    Next Steps
    - Update README
    - Can add intelligent commit messages
    - Push functionality to remote servers at regular intervals
    - Make frequent changes in a specific branch and squash commits
      into a different main branch for clean and concise history
    - Easily installable as system service for systemd
    - Cross compile to windows platform
    - deb, rpm, dmg, exe packaging
    - Support for any other version control if possible

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,79 @@
 extern crate git2;
+extern crate notify;
+
 mod git;
-use std::path::Path;
 
-fn main() {
-    // let mut repo = git::Repo::open("test_repo");
-    // let repository = &repo.repo;
-    // let files_to_commit = changed_files();
-    // let mut index = repository.index().unwrap();
-    // let file1 = index.get(0).unwrap();
-    // let file2 = index.get(1).unwrap();
-    //println!("len {},{:?},{:?},{:?},{:?}",index.len(),index.get(0).unwrap().path,index.get(1).unwrap().path,index.get(2).unwrap().path,index.get(3).unwrap().path);
-    // index.add(&file1);
-    // index.add(&file2);
-    // index.write();
-    //index.iter().for_each(e)
-    // for path in files_to_commit {
-    //     match index.add_path(Path::new(&path)) {
-    //         Ok(()) => {
-    //             println!("Succefully added path {}", path);
-    //         }
-    //         Err(e) => {
-    //             println!("Error while adding path {}",e.message());
-    //         }
-    //     }
-    // }
-    // index.write();
-    // let oid = index.write_tree().unwrap();
-    // let tree = repository.find_tree(oid).unwrap();
-    // println!("tree {:?}",tree);
+use notify::{RecommendedWatcher, Watcher, RecursiveMode};
+use std::sync::mpsc::channel;
+use std::time::Duration;
 
+extern crate timer;
+extern crate chrono;
+use std::sync::atomic::Ordering;
+use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
+use timer::Guard;
 
-    //init_repo();
-    //sample_commit();
-    add_all_changed();
+extern crate clap;
+use clap::{Arg, App};
+
+pub struct BackerConfig {
+    pub repo_path: String,
+    pub file_monitor_freq: String,
+    pub commit_delay: String,
+    pub sign_name: String,
+    pub sign_email: String,
+    pub default_commit_msg: String
 }
 
-fn init_repo() {
-    let mut repo = git::Repo::new("test_repo");
+fn main() {
+    // Name and path of repo
+    // Common commit message
+    // File watcher frequency
+    // Commit frequency
+    // Signature name
+    // Signature email
+
+    let matches = App::new("Backer").version("1.0").author("Krishna Kumar Thokala")
+        .about("A git based backup tool")
+        .arg(Arg::with_name("path").short("p").long("path").value_name("path").help("Set path to monitor").required(true))
+        .arg(Arg::with_name("ffreq").short("f").long("ffreq").value_name("ffreq").help("Set frequency for monitoring file changes (seconds)"))
+        .arg(Arg::with_name("cfreq").short("c").long("cfreq").value_name("cfreq").help("Set the delay to make commit after first file change event (seconds)"))
+        .arg(Arg::with_name("sname").short("n").long("sname").value_name("sname").help("Add name of the author").required(true))
+        .arg(Arg::with_name("semail").short("e").long("semail").value_name("semail").help("Add email of the author").required(true))
+        .arg(Arg::with_name("defcommitmsg").short("d").long("defcommitmsg").value_name("defaultcommitmsg").help("Set default commit message"))
+        .get_matches();
+    let repo_path = matches.value_of("path").unwrap().to_owned();
+    let file_monitor_freq = matches.value_of("ffreq").unwrap_or("2").to_owned();
+    let commit_delay = matches.value_of("cfreq").unwrap_or("5").to_owned();
+    let sign_name = matches.value_of("sname").unwrap().to_owned();
+    let sign_email = matches.value_of("semail").unwrap().to_owned();
+    let default_commit_msg = matches.value_of("defaultcommitmsg").unwrap_or("Committed all changes").to_owned();
+
+    let config = BackerConfig {
+        repo_path: repo_path.clone(),
+        file_monitor_freq: file_monitor_freq,
+        commit_delay: commit_delay,
+        sign_name: sign_name,
+        sign_email: sign_email,
+        default_commit_msg: default_commit_msg
+    };
+
+    init_repo(repo_path);
+    if let Err(e) = watch(config) {
+        println!("Error initializing inotify: {:?}", e)
+    }
+}
+
+fn init_repo(repo_path: String) {
+    let repo = git::Repo::new(&repo_path);
     let repository = &repo.repo;
     git::create_initial_commit(repository);
 }
 
-fn changed_files() -> Vec<String>{
-    let mut files_to_commit = Vec::new();
-    let path1 = String::from("test3");
-    let path2 = String::from("test4");
-    files_to_commit.push(path1);
-    files_to_commit.push(path2);
-    files_to_commit
-}
-
-fn sample_commit() {
-    let mut repo = git::Repo::open("test_repo");
-    let files_to_commit = changed_files();
-    match git::add_and_commit(&mut repo, files_to_commit, "Auto commit") {
+fn add_all_changed(repo_path: &str, default_commit_msg: &str, sign_name: &str, sign_email: &str) {
+    let mut repo = git::Repo::open(&repo_path);
+    match git::add_all_and_commit(&mut repo, &default_commit_msg, &sign_name, &sign_email) {
         Ok(oid) => {
             println!("Commit id {}",oid);
         },
@@ -63,16 +83,45 @@ fn sample_commit() {
     }
 }
 
-fn add_all_changed() {
-    let mut repo = git::Repo::open("test_repo");
-    match git::add_all_and_commit(&mut repo, "Auto commit all") {
-        Ok(oid) => {
-            println!("Commit id {}",oid);
-        },
-        Err(e) => {
-            println!("Unable to commit, reason {}",e.message());
+fn watch(config: BackerConfig) -> notify::Result<()> {
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+    let repo_path = config.repo_path;
+    let default_commit_msg = config.default_commit_msg;
+    let sign_name = config.sign_name;
+    let sign_email = config.sign_email;
+
+    let file_monitor_freq: u64 = config.file_monitor_freq.parse().unwrap();
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut watcher: RecommendedWatcher = try!(Watcher::new(tx, Duration::from_secs(file_monitor_freq)));
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    try!(watcher.watch(&repo_path, RecursiveMode::NonRecursive));
+
+    let time_done = Arc::new(AtomicBool::new(false));
+    let timer = timer::Timer::new();
+    let commit_delay: i64 = config.commit_delay.parse().unwrap();
+    let time_done1 = time_done.clone();
+    let callback = move || {
+        println!("Now commiting changes");
+        add_all_changed(&repo_path, &default_commit_msg, &sign_name, &sign_email);
+        time_done1.store(false, Ordering::Relaxed);
+    };
+    let mut _guard: Option<Guard>  = None;
+    loop {
+        match rx.recv() {
+            Ok(event) => {
+                println!("{:?}", event);
+                if ! time_done.load(Ordering::Relaxed) {
+                    //let time_done1 = time_done.clone();
+                    _guard = Some(timer.schedule_with_delay(chrono::Duration::seconds(commit_delay), callback.clone()));
+                    println!("Commit timer started, will be committed in {} seconds", commit_delay);
+                    time_done.store(true, Ordering::Relaxed);
+                }
+            },
+            Err(e) => println!("watch error: {:?}", e),
         }
     }
 }
-
-
